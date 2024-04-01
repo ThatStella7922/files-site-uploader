5f0d0539076b7caaf2a652443acf13b52abef413
ThatStella7922 <10524728+ThatStella7922@users.noreply.github.com>
2024-04-01 09:23:58 -0400

    Properly do validation

--- index.php
+++ index.php
@@ -17,12 +17,17 @@
             // Variable for button that deletes the uploaded file (essentially an Undo button)
             $deleteUploadedFileButton = ""; // This will be set later in the script, appears under status message
 
-            // Internal script use variables
+            // Internal script use variables for error handling
             $validationDidError = true; // Used later in this script to proceed with file upload or abort (errors, etc)
+            $didErrorFromNoInputDestinationDirectory = true; // Used 
 
+            // Check if the destination directory was provided in the form at all
             if (empty($_POST["destinationDirectory"])) {
                 // Error if destination directory was not provided in the form
                 $noDestinationDirectoryError = "Destination directory is required";
+                $statusMessage = "Could not begin file upload, destination directory is required!";
+                $validationDidError = true;
+                $didErrorFromNoInputDestinationDirectory = true;
             } else {
                 $destination_directory = "../" . $_POST["destinationDirectory"]; // Get the destination directory from the form, prepend with .. to go up one directory by default
                 $full_destination_directory = realpath($destination_directory); // Get the full path of the destination directory (from /)
@@ -33,30 +38,33 @@
                 $friendly_destination_directory[0] = "files.thatstel.la"; // replace leading / in old path with site name
                 $friendly_destination_directory = implode('/', $friendly_destination_directory); // put it back together
 
-                $validationDidError = false; // No error, proceed with upload
+                $validationDidError = false; // Destination directory was provided, proceed with upload
+                $didErrorFromNoInputDestinationDirectory = false; // Did not error, allow further path validation
             }
 
-            if (!is_dir($_POST["destinationDirectory"])){
-                if (!empty($friendly_destination_directory)) {
-                    $statusMessage = "<p>An error occurred while uploading the file:<br>Destination directory was not found: $friendly_destination_directory</p>";
+            // Check if the destination directory exists AND that the previous check did not error
+            if (is_dir($full_destination_directory) == false && $didErrorFromNoInputDestinationDirectory == false){
+                // Error if destination directory does not exist
+                $validationDidError = true;
+                if (!empty($full_destination_directory)) {
+                    $statusMessage = "<p>An error occurred while uploading the file:<br>Destination directory was not found: " . $_POST["destinationDirectory"] . " (raw path: " . $full_destination_directory . "</p>";
                 } else {
                     $statusMessage = "<p>An error occurred while uploading the file:<br>Destination directory was not found: " . $_POST["destinationDirectory"] . "</p>";
                 }
-                
             }
 
-            if (!$validationDidError) {
+            if ($validationDidError == false) {
                 // Only proceed with upload if everything checks out (no Error)
 
-                $destination_filepath = $destination_directory . "/" . basename($_FILES["inputFile"]["name"]);
-                $friendly_destination_filepath = $friendly_destination_directory . "/" .  basename($_FILES["inputFile"]["name"]);
-                $full_destination_filepath = $full_destination_directory . "/" .  basename($_FILES["inputFile"]["name"]);
+                $destination_filepath = $destination_directory . "/" . basename($_FILES["inputFile"]["name"]); // construct a destination file path
+                $friendly_destination_filepath = $friendly_destination_directory . "/" .  basename($_FILES["inputFile"]["name"]); // construct a friendly destination file path for creating a clickable url
+                $full_destination_filepath = $full_destination_directory . "/" .  basename($_FILES["inputFile"]["name"]); // construct a full destination file path from / for use with the move uploaded file php function
 
                 // Uncomment for debug information about what paths are constructed
-                // echo("<p>File path before processing: $destination_filepath<br>Full destination file path: $full_destination_filepath<br>Friendly destination file path: $friendly_destination_filepath</p><p>Provided destination directory before resolving: $destination_directory<br>Full destination directory is $full_destination_directory<br>Friendly destination directory: $friendly_destination_directory</p>");
+                //echo("<p>File path before processing: $destination_filepath<br>Full destination file path: $full_destination_filepath<br>Friendly destination file path: $friendly_destination_filepath</p><p>Provided destination directory before resolving: $destination_directory<br>Full destination directory is $full_destination_directory<br>Friendly destination directory: $friendly_destination_directory</p>");
 
                 try {
-                    move_uploaded_file($_FILES["inputFile"]["tmp_name"], $full_destination_filepath);
+                    move_uploaded_file($_FILES["inputFile"]["tmp_name"], $full_destination_filepath); // move the file from php temp to the intended destination
                     $statusMessage = "The file has been uploaded to <a href=\"https://$friendly_destination_filepath\">$friendly_destination_filepath</a>";
                 } catch (Exception $e) {
                     $statusMessage = "<p>An error occurred while uploading the file:<br>Code {$e->getCode()} - \"{$e->getMessage()}\" at line {$e->GetLine()} in {php}.</p>";
@@ -73,7 +81,7 @@
             <div id="form-container">
                 <form method="post" action="" enctype="multipart/form-data">
                     <p>Select file to be uploaded: <input type="file" name="inputFile" id="inputFile"><br>File size limit: idfk keep in mind the storage you have free on hostinger</p>
-                    <p>Destination path: <input type="text" name="destinationDirectory" id="destinationDirectory">/<br>(relative to files.thatstel.la, do NOT include trailing slash)<span class="error"><?php echo $noDestinationDirectoryError;?></span></p>
+                    <p>Destination path: <input type="text" name="destinationDirectory" id="destinationDirectory">/<br>(relative to files.thatstel.la, do NOT include trailing slash)<br><span class="error"><?php echo $noDestinationDirectoryError;?></span></p>
                      <input type="submit" value="Upload File" name="submit">
                 </form>
             </div>
